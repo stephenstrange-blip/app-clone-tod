3c16d0769e1a34bb2efdb190d4e1e642f5ace397
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-06-01 06:32:52 +0800

    Refactor: Return when nothing will be updated

--- server/api/controllers/postCtr.js
+++ server/api/controllers/postCtr.js
@@ -1,5 +1,5 @@
 const { getPost, putPost, addPost, deletePost } = require("../db/database");
-
+const _ = require("lodash");
 exports.fetchPost = async (req, res, next) => {
   try {
     const { postId } = req.params;
@@ -28,15 +28,21 @@ exports.createPost = async (req, res, next) => {
 
 exports.updatePost = async (req, res, next) => {
   try {
-    const { categoryId, title, message, published } = req.body;
-    let opts = { postId: Number(req.params.postId) };
+    const { title, categoryId, message, published } = req.body;
+    let opts = {};
 
     if (categoryId) opts["categoryId"] = categoryId;
     if (title) opts["title"] = title;
     if (message) opts["message"] = message;
     if (published) opts["published"] = published === "true" ? true : false;
 
+    if (_.isEmpty(opts))
+      return res.status(200).json({ data: { message: "Nothing to update" } });
+
+    opts = { ...opts, postId: Number(req.params.postId) };
+
     const post = await putPost(opts);
+    res.status(200).json({ data: { post } });
   } catch (err) {
     next(err);
   }
