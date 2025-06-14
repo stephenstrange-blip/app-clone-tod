d5e818a055260b6aa2d71fb90ae389acbdfb19f8
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-06-14 20:29:36 +0800

    Build: Create utils for functions used in different pages

---
+++ client/src/utils/utils.js
@@ -0,0 +1,58 @@
+export function formatDate(date_obj) {
+  let date = Date.parse(date_obj);
+  date = new Intl.DateTimeFormat(undefined, {
+    weekday: "long",
+    month: "long",
+    year: "numeric",
+    hour12: true,
+    hour: "2-digit",
+    minute: "numeric",
+    day: "2-digit",
+  }).format(date);
+
+  return date;
+}
+
+export function showBtns(e) {
+  e.currentTarget.nextElementSibling.style["display"] = "revert";
+}
+
+export function hideBtns(e) {
+  e.preventDefault();
+  e.currentTarget.parentElement.previousElementSibling.value = "";
+  return (e.currentTarget.parentElement.style["display"] = "none");
+}
+
+export const acceptRequest = async (e) => {
+  let li = e.currentTarget.parentElement;
+  let input = {
+    targetId: li.dataset.targetid,
+    requesterId: li.dataset.requesterid,
+  };
+
+  const response = removeRequest(input);
+  const result = createNetwork({ followerId: input.requesterId });
+  const [_response, _result] = await Promise.allSettled([response, result]);
+
+  console.log(_response, _result);
+  return setTimeout(() => window.location.reload(), 500);
+};
+
+export const rejectRequest = async (e) => {
+  let li = e.currentTarget.parentElement;
+  let input = {
+    targetId: li.dataset.targetid,
+    requesterId: li.dataset.requesterid,
+  };
+
+  const response = removeRequest(input);
+  const _response = await Promise.allSettled([response]);
+  console.log(_response);
+
+  return setTimeout(() => window.location.reload(), 1500);
+};
+
+export const REGEX = {
+  POSTID: new RegExp(/^[0-9]+$/),
+};
+export const FULFILLED = ["fulfilled", 200];
