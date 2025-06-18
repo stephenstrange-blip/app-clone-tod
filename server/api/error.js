3c5ca58c4d579a835edab7d0ae980f31be16671f
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-06-18 09:24:03 +0800

    Chore: Get origin via environment

--- server/api/error.js
+++ server/api/error.js
@@ -1,9 +1,10 @@
 const prisma = require("./db/prisma").prisma;
+require("dotenv").config();
 
 exports.error = async (err, req, res, next) => {
   console.log("ERROR!!");
   console.dir(err.stack);
   await prisma.$disconnect();
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
   res.status(500).json({ message: err.message, stack: err.stack });
 };
