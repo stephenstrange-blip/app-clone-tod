ddc4f1707a74e29502c7ac8a74929112c9232d09
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-06-20 06:10:55 +0800

    Fix: Fix the use of environment variable

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -3,11 +3,8 @@ import axios from "axios";
 const env = import.meta.env;
 
 const apiClient = axios.create({
-  baseURL: env?.RAILWAY_PUBLIC_DOMAIN
-    ? import.meta.env.BASE_SERVER_URL
-    : "/api",
+  baseURL: env?.PROD ? env.VITE_BASE_SERVER_URL : "/api",
   httpAgent: false,
-  // httpsAgent: false,
   headers: {
     "Content-Type": "application/json",
   },
