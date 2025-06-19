8c2439b922657021bf11cafc87f137f3e9e1bf36
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-06-20 06:13:28 +0800

    Chore: Remove setHeader & ignore some files

--- server/api/error.js
+++ server/api/error.js
@@ -5,6 +5,6 @@ exports.error = async (err, req, res, next) => {
   console.log("ERROR!!");
   console.dir(err.stack);
   await prisma.$disconnect();
-  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+
   res.status(500).json({ message: err.message, stack: err.stack });
 };
