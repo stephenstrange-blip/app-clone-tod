b6667132fec7967b626f745fc3916f0be1517471
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-06-23 22:22:21 +0800

    Feat: Adjust cookie options to match cookie options in logout

--- server/api/db/jwt.js
+++ server/api/db/jwt.js
@@ -16,7 +16,7 @@ exports.signToken = async (req, res, next) => {
 
   if (username) {
     opts = { id, username };
-    console.log(`Logging in via local: Username: ${username}, id: ${id}`);
+    console.log(`Logging in via local: Username: ${username},, id: ${id}`);
   }
 
   if (githubId) {
@@ -30,13 +30,25 @@ exports.signToken = async (req, res, next) => {
     {
       algorithm: "HS256",
       expiresIn: "1d",
-      audience: "http://localhost:8080",
-      issuer: "http://localhost:8080",
+      audience: process.env.FRONTEND_URL,
+      issuer: process.env.BASE_SERVER_URL,
     },
     (err, token) => {
+      console.log("Token received");
       if (err) next(err, false);
       else {
-        res.status(200).json({ token });
+        console.log("Setting a cookie from the token");
+        res
+          .status(201)
+          .cookie("token", token, {
+            httpOnly: true,
+            signed: true,
+            maxAge: 24 * 60 * 60 * 1000, // ms,
+            secure: process.env.NODE_ENV === "production", // true if using HTTPS
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            // domain: process.env.BASE_SERVER_URL, -> Domain name for the cookie. Defaults to the domain name of the app.
+          })
+          .redirect(process.env.FRONTEND_URL + "/home");
       }
     }
   );
