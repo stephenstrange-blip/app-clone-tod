abb3107f1aadc573b277753c742395afae0256e5
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-06-23 22:23:35 +0800

    Refactor: Check for cookies to authorize user and clear if unauthorized

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = new Router();
-
+const __isEmpty = require("lodash").isEmpty;
 const passport = require("../passport/passport").passport;
 
 const { error } = require("../error");
@@ -12,19 +12,44 @@ const requestRouter = require("./request").router;
 const networkRouter = require("./network").router;
 const reactionRouter = require("./reaction").router;
 
-router.use(function (req, res, next) {
-  passport.authenticate("jwt", { session: false }, (err, user, info) => {
-    if (err) return next(err);
+let cookieOptions = {
+  httpOnly: true,
+  signed: true,
+  secure: process.env.NODE_ENV === "production", // true if using HTTPS
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+};
 
-    // TODO: IMPLEMENT AN APPROPRIATE RESPONSE BECAUSE THE CURRENT ERROR IS 'BAD RESPONSE'
-    // WHEN IT SHOULD BE RELATED TO UNAUTHORIZED
-    if (!user) return next(new Error("User does not exist!"));
+router.use(function (req, res, next) {
+  passport.authenticate("jwt", { session: false }, (err, user) => {
+    if (err || !user) {
+      const cookies = !__isEmpty(req.cookies) && Object.keys(req.cookies);
+      const signedCookies =
+        !__isEmpty(req.signedCookies) && Object.keys(req.signedCookies);
+
+      if (cookies) {
+        console.log("Clearing unsigned cookies");
+        for (let name of cookies) res.clearCookie(name, cookieOptions);
+      }
+
+      if (signedCookies) {
+        console.log("Clearing signed cookies");
+        for (let name of signedCookies) res.clearCookie(name, cookieOptions);
+      }
+
+      return res.status(401).json({
+        message: !user ? "User does not exist" : "Unauthorized Access",
+      });
+    }
 
     req.user = user;
     next();
     // req.login(user, next); -> no need to call this as jwt is not session-based and needs a de/serializer from passport local
   })(req, res, next);
 });
+
+router.get("/auth/me", (req, res, next) =>
+  res.json({ authenticated: !!req.user })
+);
 
 router.use("/profile", profileRouter);
 router.use("/posts", postRouter);
