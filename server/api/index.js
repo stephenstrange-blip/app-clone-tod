8110f4387220d4671032833edc20f9e06554e5e2
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-06-23 22:04:43 +0800

    Chore: Update import directory

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const signup = require("./routes/signup").router;
 const login = require("./routes/login").router;
 const logout = require("./routes/logout").router;
-const github = require("./routes/github").router;
+const github = require("./github/github").router;
 const user = require("./routes/user").router;
 
 module.exports = { signup, login, logout, github, user };
