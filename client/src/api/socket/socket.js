7e8df379bf946639b96e22adebc4414e5d7dec4a
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-07-01 18:49:12 +0800

    Build: Separate data transmission with namespace

--- client/src/api/socket/socket.js
+++ client/src/api/socket/socket.js
@@ -1,5 +1,9 @@
 import { io } from "socket.io-client";
+const NAMESPACES = {
+  chatroom: "chats",
+};
 
+// main namespace
 export const socket = io(import.meta.env.VITE_BASE_SERVER_URL, {
   autoConnect: false,
   withCredentials: true,
@@ -7,4 +11,20 @@ export const socket = io(import.meta.env.VITE_BASE_SERVER_URL, {
   auth: {
     serverOffset: 0,
   },
+  retries: 5,
+  ackTimeout: 8000,
 });
+
+export const chatSocket = io(
+  `${import.meta.env.VITE_BASE_SERVER_URL}/${NAMESPACES.chatroom}`,
+  {
+    autoConnect: false,
+    withCredentials: true,
+    path: "/chatroom/",
+    auth: {
+      serverOffset: 0,
+    },
+    retries: 5,
+    ackTimeout: 8000,
+  }
+);
