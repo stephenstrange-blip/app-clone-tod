d14b709724b5a605244cdfc0504e3f2409c27ed9
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-07-01 18:54:44 +0800

    Build: Connect chatpage and chatroom to react router routes

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -7,7 +7,9 @@ import Home, { loader as homeLoader } from "../pages/Home";
 import Root from "../pages/Root";
 import PostPage, { loader as postPageLoader, action as postAction } from "../pages/Post";
 import Profile, { loader as profileLoader, action as profileAction } from "../pages/Profile";
-import Chatroom, { loader as chatRoomLoader } from "../pages/Chatroom";
+
+import ChatPage, { loader as chatPageLoader } from "../pages/ChatPage";
+import ChatRoom, { loader as chatRoomLoader, action as chatRoomAction } from "../pages/components/Chatroom";
 
 export const routes = createRoutesFromElements(
 
@@ -17,18 +19,29 @@ export const routes = createRoutesFromElements(
     <Route element={<Signin />} path={"signin"} loader={signinLoader} action={async ({ request }) => run({ request, routeFunc: signinAction, errorRedirect: '/signin' })} />
     <Route element={<Signup />} path={"signup"} loader={signupLoader} action={async ({ request }) => run({ request, routeFunc: signupAction, errorRedirect: '/signup' })} />
     <Route element={<Profile />} path={"home/profile"} loader={async ({ params }) => run({ params, routeFunc: profileLoader, errorRedirect: '/home' })} action={async ({ request }) => run({ request, routeFunc: profileAction, errorRedirect: '/home' })} />
-    <Route element={<PostPage />} path={'home/:postId'} loader={async ({ params }) => run({ params, routeFunc: postPageLoader, errorRedirect: '/home' })} action={async ({ request }) => run({ request, routeFunc: postAction, errorRedirect: '/home' })} />
-    <Route element={<Chatroom />} path={"chatroom"} loader={async({params}) => run({params, routeFunc: chatRoomLoader, errorRedirect: '/home'})}></Route>
+    <Route element={<PostPage />} path={"home/:postId"} loader={async ({ params }) => run({ params, routeFunc: postPageLoader, errorRedirect: '/home' })} action={async ({ request }) => run({ request, routeFunc: postAction, errorRedirect: '/home' })} />
+    <Route element={<ChatPage />} path={"chatroom"} loader={async ({ params }) => run({ params, routeFunc: chatPageLoader, errorRedirect: '/home' })}>
+      <Route element={<ChatRoom />} path={"/chatroom/:chatId"} loader={async ({ params }) => run({ params, routeFunc: chatRoomLoader, errorRedirect: '/chatroom' })}
+        action={async ({request, params}) => {
+          return run({ request, params, routeFunc: chatRoomAction, errorRedirect: '/chatroom' })
+        }}>
+      </Route>
+    </Route>
   </Route>
 )
 
 async function run({ request, params, routeFunc, errorRedirect }) {
+  let result;
+
   try {
-    const result = await routeFunc(request ? { request } : { params });
+    if (request && params) result = await routeFunc({ request, params })
+    else result = await routeFunc(request ? { request } : { params });
+
     if (result) return result
   } catch (err) {
     console.error(err);
     console.log(`Redirecting to ${errorRedirect}`)
+
     await new Promise(resolve => setTimeout(resolve, 3000));
     return redirectDocument(errorRedirect)
   }
